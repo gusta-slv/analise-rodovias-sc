6f0464827c0ff51aad7c5c5a545f4b349e1757a5
Gustavo Silva <gustavodasiilva@gmail.com>
2025-07-02 23:31:03 -0300

    Extraction from highways at state documentation done

--- src/extract_state_data.py
+++ src/extract_state_data.py
@@ -1,7 +1,19 @@
 import tabula
 import pandas as pd
-dfs = tabula.read_pdf(r"C:/Users/gusta/Git/projetos/rodovias_files/Relatorio_SRE2024_SC-1.pdf", pages=(20))
-print(dfs)
-print(type(dfs))
-print(type(dfs[0]))
-print(dfs[0])
+#Importar páginas do PDF a serem extraídos os dados
+dfs = tabula.read_pdf(r"C:/Users/gusta/Git/projetos/rodovias_files/Relatorio_SRE2024_SC-1.pdf", pages=('20-28'), multiple_tables=False)
+sc_fed = dfs[0]
+#Renomeando colunas
+sc_fed.columns = ['Código do trecho', 'Início do trecho', 'Fim do trecho', 'Início (km)', 'Fim (km)', 'Ext. (km)', 'Situação física', 'Estadual coincid.', 'Federal superp.', 'TMD']
+#Removendo linhas que começam com "CÓDIGO DO" e "TRECHO"
+sc_fed = sc_fed[sc_fed['Código do trecho'] != 'CÓDIGO DO']
+sc_fed = sc_fed[sc_fed['Código do trecho'] != 'TRECHO']
+#Resetando index após filtrar os valores
+sc_fed.reset_index(drop=True, inplace=True)
+del dfs
+dfs = tabula.read_pdf(r"C:/Users/gusta/Git/projetos/rodovias_files/Relatorio_SRE2024_SC-1.pdf", pages=('29-51'), multiple_tables=False)
+sc_est = dfs[0]
+sc_est.columns = ['Código do trecho', 'Início do trecho', 'Fim do trecho', 'Início (km)', 'Fim (km)', 'Ext. (km)', 'Situação física', 'Tipo revest.', 'Estadual coincid.', 'Federal superp.', 'TMD']
+sc_est = sc_est[sc_est['Código do trecho'] != 'CÓDIGO DO']
+sc_est = sc_est[sc_est['Código do trecho'] != 'TRECHO']
+sc_est.reset_index(drop=True, inplace=True)
