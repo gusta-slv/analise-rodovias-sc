82cdcb23c74e9374675982360bc886b06e881e14
Gustavo Silva <gustavodasiilva@gmail.com>
2025-07-09 23:09:27 -0300

    First try to extract coordinates

--- src/extract_coord.py
+++ src/extract_coord.py
@@ -1,1 +1,10 @@
 #Code to extract coordinates for federal highways in SC
+import geopandas as gpd
+from shapely.geometry import Point, Polygon
+import pandas as pd
+import fiona
+fiona.drvsupport.supported_drivers['KML'] = 'rw'
+my_map = gpd.read_file('C:/Users/gusta/Git/projetos/rodovias_files/snv_202504A.kml', driver='KML')
+my_map
+df_map = pd.DataFrame(my_map)
+df_map
